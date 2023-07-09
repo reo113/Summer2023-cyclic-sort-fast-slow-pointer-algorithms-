75725883db9e4c39d433a603c0cd11ec6028edf4
reo113 <roberteliortiz@gmail.com>
2023-07-09 13:21:46 -0400

    !

--- subarraySum.js
+++ subarraySum.js
@@ -1,22 +1,38 @@
 //Set B #6
-
-
+    // Pseudo code:
+    // Initialize a count variable to keep track of the number of subarrays that sum up to k
+    // Initialize a total variable to store the current sum of elements in the subarray
+    // Set the end variable to the last index of the array
 function subArray(arr, k) {
-  let count = 0;
-  let total = 0;
-  let end = arr.length - 1;
-
-  for (let i = 0; i <= end; i++) {
-    let j = i + 1;
-    total = arr[i] + arr[j];
-    if (arr[i] === k) count++;
-    while (j <= end && total <= k || total >= k) {
-      if (total === k) count++;
-      total += arr[++j];
+    let count = 0;
+    let total = 0;
+    let end = arr.length - 1;
+    for (let i = 0; i <= end; i++) {
+      // Set j to the next index of i
+      let j = i + 1;
+      // Calculate the initial total by adding the current element and the next element
+      total = arr[i] + arr[j];
+      
+      // Check if the current element itself is equal to k
+      if (arr[i] === k)
+        count++;
+      
+      // Continue iterating while j is within the array bounds and the total is less than or equal to k
+      // or greater than k
+      while (j <= end && (total <= k || total >= k)) {
+        // If the total is equal to k, increment the count
+        if (total === k)
+          count++;
+        
+        // Increment j and add the next element to the total
+        total += arr[++j];
+      }
     }
+  
+    // Return the count of subarrays that sum up to k
+    return count;
   }
-  return count;
-}
+  
 
 nums = [1, 1, 1];
 nums2 = [1, 2, 3];
